9fb24567cf9f5daa50be2a99d109ee16a49eceee
bobyo <bobyourhumanhelper@gmail.com>
2020-03-27 11:57:51 +0300

    Merge remote-tracking branch 'gojo/master'
    # Conflicts:
    #	app/src/main/java/codoulingo/codoulingo/dgsgds.java

--- app/src/main/java/codoulingo/codoulingo/MainActivity.java
+++ app/src/main/java/codoulingo/codoulingo/MainActivity.java
@@ -10,6 +10,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,20 +23,19 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Lessons").child("1");
         myRef.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String vals = "";
+                String folder_main = dataSnapshot.child("LessonID").getValue(String.class) + dataSnapshot.child("LessonName").getValue(String.class);
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 for (DataSnapshot snap : dataSnapshot.getChildren()) {
                     if (snap.getValue(String.class).length() != 0) {
                         Log.d("VAL", "Value is: " + snap.getKey() + "    " + snap.getValue(String.class));
-                        vals += snap.getValue(String.class) + "\n";
+                        ReadWrite.write(folder_main + snap.getKey(), snap.getValue(String.class), MainActivity.this);
                     }
                 }
-                Log.d(dataSnapshot.child("LessonID").getValue(String.class), vals);
-                ReadWrite.write(dataSnapshot.child("LessonID").getValue(String.class), vals, MainActivity.this);
-                Log.d("hi", ReadWrite.read(dataSnapshot.child("LessonID").getValue(String.class), MainActivity.this));
             }
 
             @Override
@@ -44,4 +45,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        }}
+        }
+        public HashMap<String, String> readqs(String id, String name, String qs_num) {
+            String content = ReadWrite.read(id + name + "qs" + qs_num, MainActivity.this);
+            HashMap<String, String> hashMap = new HashMap<>();
+            String[] arr = content.split("\\[|\\]");
+            hashMap.put("type", arr[0]);
+            hashMap.put("Content", arr[1]);
+            hashMap.put("Image", arr[2]);
+            hashMap.put("Answer", arr[3]);
+            return hashMap;
+        }
+
+}
